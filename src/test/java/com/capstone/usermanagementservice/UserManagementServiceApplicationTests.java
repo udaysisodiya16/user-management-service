f0490085e21f06959d1ebbbcf4aff51ac1f7fea1
Uday Sisodiya <uday.sisodiya@agshealth.com>
2024-10-23 12:24:44 +0530

    Java Version changed

--- src/test/java/com/capstone/usermanagementservice/UserManagementServiceApplicationTests.java
+++ src/test/java/com/capstone/usermanagementservice/UserManagementServiceApplicationTests.java
@@ -1,13 +1,12 @@
 package com.capstone.usermanagementservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UserManagementServiceApplicationTests {
 
-	@Test
+/*	@Test
 	void contextLoads() {
-	}
+	}*/
 
 }

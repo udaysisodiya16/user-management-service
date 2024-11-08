ff5e6d909f1e96679122bbf782058a97c610db69
Uday Sisodiya <uday.sisodiya@agshealth.com>
2024-11-08 12:36:29 +0530

    Authentication changes

--- src/main/java/com/capstone/usermanagementservice/auth/SpringSecurity.java
+++ src/main/java/com/capstone/usermanagementservice/auth/SpringSecurity.java
@@ -1,35 +1,20 @@
 package com.capstone.usermanagementservice.auth;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
 @Component
 @EnableWebSecurity
 public class SpringSecurity {
 
-    @Autowired
-    private JwtAuthenticationFilter jwtAuthenticationFilter;
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable() // Disable CSRF for API
-                .authorizeHttpRequests(auth -> auth
-                        // Allow public access to login and signup APIs
-                        .requestMatchers("/login", "/signup").permitAll()
-                        // Secure all other endpoints
-                        .anyRequest().authenticated()
-                )
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Stateless, JWT-based authentication
-                .and()
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); // Add JWT filter
-        return http.build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors().disable();
+        httpSecurity.csrf().disable();
+        httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+        return httpSecurity.build();
     }
 }
